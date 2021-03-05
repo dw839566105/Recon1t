dc2a197df3651361822b24e2f872eea31e136534
Dou Wei <839566105@qq.com>
2021-03-05 15:33:25 +0800

    Add time

--- calib/PE_calib/main.py
+++ calib/PE_calib/main.py
@@ -44,6 +44,9 @@ def main_Calib(filename, output, mode, alg, basis, order, figure, verbose, offse
         counts = counts[counts!=0]
         PMTPosRep = PMTPos[ChannelID]
         vertex = np.repeat(VertexTruth, counts, axis=0)
+    elif mode == 'combined':
+        PMTPosRep = np.tile(PMTPos, (EventNo,1))
+        vertex = np.repeat(VertexTruth, PMTNo, axis=0)
 
     if basis == 'Legendre':
         X, cos_theta = pub.LegendreCoeff(PMTPosRep, vertex, order, Legendre=True)
@@ -69,11 +72,35 @@ def main_Calib(filename, output, mode, alg, basis, order, figure, verbose, offse
         y = Q
     elif mode == 'time':
         y = PulseTime 
+    elif mode == 'combined':
+        PulseTime = PulseTime/np.max(PulseTime)
+        bins = np.arange(0,1,0.01)
+
+        N = 10
+        x = pub.legval(bins, np.eye(N).reshape(N, N, 1))
+        Y = np.tile(x, len(np.unique(EventID))*len(np.unique(ChannelID))).T
+        Y = np.vstack((Y, Y))
+        #basis = np.zeros_like(X)
+        
+        X = np.repeat(X, bins.shape[0], axis=0)
+        y = np.zeros((len(np.unique(EventID)), len(np.unique(ChannelID)), len(bins)))
+
+        for k_index, k in enumerate(np.unique(EventID)): # event begin with 1
+            if not k % 100:
+                print(k)
+            index = EventID == k
+            CID = ChannelID[index]
+            Pulse_t = PulseTime[index]
+            for i in np.unique(CID): # PMT begin with 0
+                y[k_index, i, 1:], _ = np.histogram(Pulse_t[CID==i], bins=bins)
+        y = np.reshape(y,(-1))
+
     # symmetry
     if basis == 'Zernike':
         y = np.hstack((y, y))
     else:
         pass
+    print(y.shape,X.shape)
 
     if verbose:
         print(f'the basis shape is {X.shape}, and the dependent variable shape is {y.shape}') 
@@ -81,50 +108,70 @@ def main_Calib(filename, output, mode, alg, basis, order, figure, verbose, offse
     # Regression methods:
     if (alg == 'sms'):
         import statsmodels.api as sm
-        if mode == 'PE':
-            model = sm.GLM(y, X, family=sm.families.Poisson())
+        if mode != 'combined':
+            if mode == 'PE':
+                model = sm.GLM(y, X, family=sm.families.Poisson())
+                result = model.fit()
+
+                if verbose:
+                    print(result.summary())
+                AIC = result.aic
+                coef_ = result.params
+                std = result.bse
+
+            elif mode == 'time':
+                import pandas as pd
+                data = pd.DataFrame(data = np.hstack((X, np.atleast_2d(y).T)))                
+                strs = 'y ~ '
+                start = data.keys().start
+                stop = data.keys().stop
+                step = data.keys().step
+
+                cname = []
+                cname.append('X0')
+                for i in np.arange(start+1, stop, step):
+                    if i == start + 1:
+                        strs += 'X%d ' % i
+                    elif i == stop - step:
+                        pass
+                    else:
+                        strs += ' + X%d ' % i                      
+
+                    if i == stop - step:
+                        cname.append('y')
+                    else:
+                        cname.append('X%d' % i)
+                data.columns = cname
+
+                mod = sm.formula.quantreg(strs, data[cname])
+
+                res = mod.fit(q=0.1,)
+                coef_ = res.params
+                AIC = np.zeros_like(coef_)
+                std = np.zeros_like(coef_)
+        elif mode == 'combined':
+
+            basis = np.zeros((X.shape[0], X.shape[1]*Y.shape[1]))
+
+            for i_index, i in enumerate(np.arange(X.shape[1])):
+                for j_index, j in enumerate(np.arange(Y.shape[1])):
+                    total_index = i_index*Y.shape[1] + j_index
+                    if not total_index % 10:
+                        print(total_index)
+                    basis[:, total_index] = X[:,i_index]*Y[:,j_index]
+            # data = pd.DataFrame(data = np.hstack((basis, np.atleast_2d(y).T)))                
+            print('begin...')
+            model = sm.GLM(y, basis, family=sm.families.Poisson())
             result = model.fit()
 
             if verbose:
                 print(result.summary())
             AIC = result.aic
-            coef_ = result.params
-            std = result.bse
 
-        elif mode == 'time':
-            import pandas as pd
-            data = pd.DataFrame(data = np.hstack((X, np.atleast_2d(y).T)))                
-            strs = 'y ~ '
-            start = data.keys().start
-            stop = data.keys().stop
-            step = data.keys().step
-
-            cname = []
-            cname.append('X0')
-            for i in np.arange(start+1, stop, step):
-                if i == start+1:
-                    strs += 'X%d ' % i
-                elif i == stop-step:
-                    pass
-                else:
-                    strs += ' + X%d ' % i                      
-
-                if i == stop-step:
-                    cname.append('y')
-                else:
-                    cname.append('X%d' % i)
-            data.columns = cname
-
-            mod = sm.formula.quantreg(strs, data[cname])
-
-            res = mod.fit(q=0.1,)
-            coef_ = res.params
-            AIC = np.zeros_like(coef_)
-            std = np.zeros_like(coef_)
             print('Waring! No AIC and std value')
-            if verbose:
-                print(f'rank: {np.linalg.matrix_rank(data[cname])}')
-                print(res.summary())
+        if verbose:
+            print(f'rank: {np.linalg.matrix_rank(data[cname])}')
+            print(res.summary())
 
     elif (alg == 'custom'):
         from scipy.optimize import minimize
@@ -152,7 +199,7 @@ def main_Calib(filename, output, mode, alg, basis, order, figure, verbose, offse
         # print(std)
         print('Waring! No AIC and std value, std is testing')
 
-    elif (alg == 'sk'):            
+    elif alg == 'sk':
         from sklearn.linear_model import TweedieRegressor
         alpha = 0.001
         reg = TweedieRegressor(power=1, alpha=alpha, link='log', max_iter=1000, tol=1e-6, fit_intercept=False)
@@ -169,38 +216,48 @@ def main_Calib(filename, output, mode, alg, basis, order, figure, verbose, offse
         std = np.zeros_like(coef_)
         print('Waring! No AIC and std value')
 
-    elif (alg == 'h2o'):
+    elif alg == 'h2o':
         import h2o
         from h2o.estimators.gbm import H2OGradientBoostingEstimator
         from h2o.estimators.glm import H2OGeneralizedLinearEstimator           
+        if mode != 'combined':
+            y = np.atleast_2d(y).T
+            data = np.hstack((X, y, np.ones_like(y)))
 
-        y = np.atleast_2d(y).T
-        data = np.hstack((X, y, np.ones_like(y)))
+            h2o.init() 
+            hf = h2o.H2OFrame(data)
+            predictors = hf.columns[0:-2]
+            response_col = hf.columns[-2]
 
-        h2o.init() 
-        hf = h2o.H2OFrame(data)
-        predictors = hf.columns[0:-2]
-        response_col = hf.columns[-2]
+            if mode == 'PE':
+                #offset_col = hf.columns[-1]
+                glm_model = H2OGeneralizedLinearEstimator(family= "poisson",
+                    #offset_column = offset_col, 
+                    lambda_ = 0,
+                    compute_p_values = True)
 
-        if mode == 'PE':
-            #offset_col = hf.columns[-1]
-            glm_model = H2OGeneralizedLinearEstimator(family= "poisson",
-                #offset_column = offset_col, 
-                lambda_ = 0,
-                compute_p_values = True)
+                glm_model.train(predictors, response_col, training_frame=hf)
 
-            glm_model.train(predictors, response_col, training_frame=hf)
+                coef_table = glm_model._model_json['output']['coefficients_table']
+                coef_ = glm_model.coef()
 
-            coef_table = glm_model._model_json['output']['coefficients_table']
-            coef_ = glm_model.coef()
+            elif mode == 'time':
+                gbm = H2OGradientBoostingEstimator(distribution="quantile", seed = 1234,
+                                                  stopping_metric = "mse", stopping_tolerance = 1e-4)
+                gbm.train(x = predictors, y = response_col, training_frame = hf)
+                breakpoint()
+                print(gbm)
+                exit()
+        elif mode == 'combined':
+            y = np.atleast_2d(y).T
+            data = np.hstack((X, Y, y, np.ones_like(y)))
 
-        else:
-            gbm = H2OGradientBoostingEstimator(distribution="quantile", seed = 1234,
-                                              stopping_metric = "mse", stopping_tolerance = 1e-4)
-            gbm.train(x = predictors, y = response_col, training_frame = hf)
+            h2o.init() 
+            hf = h2o.H2OFrame(data)
             breakpoint()
-            print(gbm)
-            exit()
+            predictors = hf.columns[0:-2]
+            response_col = hf.columns[-2]
+            
 
         if verbose:
             print(coef_)
@@ -243,7 +300,7 @@ parser.add_argument('-o', '--output', dest='output', metavar='output[*.h5]', typ
 parser.add_argument('-v', '--verbose', type=int, choices=[0,1], default=0, 
                     help='Output debug info')
 
-parser.add_argument('--mode', dest='mode', type=str, choices=['PE', 'time'], default='PE',
+parser.add_argument('--mode', dest='mode', type=str, choices=['PE', 'time','combined'], default='PE',
                     help='Which info should be used')
 
 parser.add_argument('--alg', dest='alg', type=str, choices=['sms', 'sk', 'h2o', 'custom'], default='sms',
@@ -253,14 +310,14 @@ parser.add_argument('--alg', dest='alg', type=str, choices=['sms', 'sk', 'h2o', 
 
 parser.add_argument('--basis', dest='basis', type=str, choices=['Legendre', 'Zernike'],default='Legendre',
                     help=textwrap.dedent('''Basis to be used in regression.
-                    Legendre    -  For data with the same radius. (1d)
+                    Legendre  -  For data with the same radius. (1d)
                     Zernike  -  For data random distributed. (2d)'''))
 
 parser.add_argument('--order', dest='order', metavar='N', type=int, default=10,
                     help=textwrap.dedent('''The max cutoff order. 
                     For Zernike is (N+1)*(N+2)/2'''))
 
-parser.add_argument('--figure', dest='figure', type=str, choices=['ON', 'OFF'], default='OFF',
+parser.add_argument('--figure', dest='figure', metavar='filename[*.png]', type=str, default=False,
                     help=textwrap.dedent('''Whether plot figure, default is "OFF".
                     Figure is a unit diskonly effect for Zernike now'''))
 
